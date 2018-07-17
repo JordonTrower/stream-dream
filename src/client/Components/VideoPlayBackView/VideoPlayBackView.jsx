4169c1a7faed967cdb4e9080ec1882ac2af3ac28
Trevor Smith <t.smithno2@gmail.com>
2018-07-17 12:16:25 -0600

    created base for videoplayback view

---
+++ src/client/Components/VideoPlayBackView/VideoPlayBackView.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class VideoPlayBackView extends Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
+	componentDidMount() {
+		console.log(this.state);
+	}
+
+	render() {
+		return (
+			<div className="Container">
+				<h1>VIDEO PLAYBACK VIEEEEEWWWWWWW!!!!!!!!!!!!!!!!!!!</h1>
+			</div>
+		);
+	}
+}
