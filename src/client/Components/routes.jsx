9bb4c6a2d2e97dc4d1e425a11f157c6e7cdadc60
James 'Mac' Read <james.macgregor.read@gmail.com>
2018-07-17 16:53:41 -0600

    Initial pass at UploadedVideos component

--- src/client/Components/routes.jsx
+++ src/client/Components/routes.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './home/Home'
+import Home from './home/Home';
+import Control from './control/Control';
 
 export default (
 	<Switch>
 		<Route path = "/" component = { Home } exact />
+		<Route path = "/control" component = { Control } />
 	</Switch>
 )
---
+++ src/client/styled/common/cardbody.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import commonCSS from './commonCSS';
+
+export default styled.div `
+
+	${commonCSS.textColor}
+	${commonCSS.flex()}
+    
+    width: 50rem;
+    height: 50rem;
+    border: 1px solid black;
+	flex-wrap: wrap;
+`
