28b71e4e52da1d624296cff0ec7be3ab1bae28ed
Jordon <leopardclone@gmail.com>
2018-07-17 16:00:12 -0600

    Finished registration tests, added deleting of accounts

--- migrations/20180717104454_create_tables.js
+++ migrations/20180717104454_create_tables.js
@@ -1,51 +1,64 @@
+exports.up = function (knex, Promise) {
+	return knex.schema.createTable('users', (table) => {
+			table.increments();
+			table.string('email', 50);
+			table.string('password', 62);
+			table.string('displayName', 26);
+			table.string('avatar', 255);
+			table.boolean('online');
+			table.timestamps(false, true);
 
-exports.up = function(knex, Promise) {
-    return knex.schema.createTable('users', (table) =>{
-        table.increments();
-        table.string('user', 50);
-        table.string('password', 62);
-        table.string('displayName', 26);
-        table.string('avatar',255);
-        table.boolean('online');
-    })
-    
-    .then ( () => knex.schema.createTable('games', (table) => {
-        table.increments();
-        table.string('title', 100);
-        table.string('picture', 255);
-    }))
+			table.unique(['email', 'displayName'])
+		})
 
-    .then ( () => knex.schema.createTable('videos', (table) => {
-        table.increments();
-        table.integer('userid').references('users.id')
-        table.integer('gameid').references('games.id')
-        table.string('title', 100);
-        table.string('link', 255);
-        table.timestamps(false, true) // setting to Date-Time type value and using current time when saving
-    }))
+		.then(() => knex.schema.createTable('games', (table) => {
+			table.increments();
+			table.string('title', 100);
+			table.string('picture', 255);
+			table.timestamps(false, true);
+		}))
 
-    .then ( () => knex.schema.createTable('streams', (table) => {
-        table.increments();
-        table.integer('userid').references('users.id');
-        table.integer('videoid').references('videos.id');
-        table.integer('gameid').references('games.id');
-        table.string('title', 100);
-      }))
+		.then(() => knex.schema.createTable('videos', (table) => {
+			table.increments();
+			table.integer('userid').references('users.id')
+			table.integer('gameid').references('games.id')
+			table.string('title', 100);
+			table.string('link', 255);
+			table.timestamps(false, true) // setting to Date-Time type value and using current time when saving
+		}))
 
-    .then ( () => knex.schema.createTable('followings', (table) => {
-        table.increments();
-        table.integer('userid').references('users.id'); //the person who is doing the following
-        table.integer('followingid').references('users.id'); //the user the person is following
-    }))
+		.then(() => knex.schema.createTable('streams', (table) => {
+			table.increments();
+			table.integer('userid').references('users.id');
+			table.integer('videoid').references('videos.id');
+			table.integer('gameid').references('games.id');
+			table.string('title', 100);
+			table.timestamps(false, true);
+		}))
 
-    .then ( () => knex.schema.createTable('comments', (table) => {
-        table.increments();
-        table.integer('userid').references('users.id');
-        table.integer('videoid').references('videos.id');
-        table.string('comment',255);
-    }))
+		.then(() => knex.schema.createTable('followings', (table) => {
+			table.increments();
+			table.integer('userid').references('users.id'); //the person who is doing the following
+			table.integer('followingid').references('users.id'); //the user the person is following
+			table.timestamps(false, true);
+		}))
+
+		.then(() => knex.schema.createTable('comments', (table) => {
+			table.increments();
+			table.integer('userid').references('users.id');
+			table.integer('videoid').references('videos.id');
+			table.string('comment', 255);
+			table.timestamps(false, true);
+		}))
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+	return knex.schema.dropTableIfExists('comments')
+		.then(() => knex.schema.dropTableIfExists('followings'))
+		.then(() => knex.schema.dropTableIfExists('streams'))
+		.then(() => knex.schema.dropTableIfExists('videos'))
+		.then(() => knex.schema.dropTableIfExists('games'))
+		.then(() => knex.schema.dropTableIfExists('users'));
+
+	console.log(knex);
 };
--- src/client/Components/App.jsx
+++ src/client/Components/App.jsx
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
-import route from './routes';
+import React from 'react';
+import Route from './routes';
 
 import './App.css';
 
 function App() {
-	render() {
-		return (
-			<div className="App">
-				<route />
-			</div>
-		);
-	}
+	return (
+		<div className="App">
+			<Route />
+		</div>
+	);
 }
 
 export default App;
