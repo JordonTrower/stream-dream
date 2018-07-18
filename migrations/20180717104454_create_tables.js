5d0787b052f716918e32562f0ce9b43bef3ebacc
Jordon <leopardclone@gmail.com>
2018-07-18 09:48:34 -0600

    Made seed and updated migration

--- migrations/20180717104454_create_tables.js
+++ migrations/20180717104454_create_tables.js
@@ -3,12 +3,12 @@ exports.up = function (knex, Promise) {
 			table.increments();
 			table.string('email', 50);
 			table.string('password', 62);
-			table.string('displayName', 26);
+			table.string('display_name', 26);
 			table.string('avatar', 255);
 			table.boolean('online');
 			table.timestamps(false, true);
 
-			table.unique(['email', 'displayName'])
+			table.unique(['email', 'display_name'])
 		})
 
 		.then(() => knex.schema.createTable('games', (table) => {
@@ -20,8 +20,8 @@ exports.up = function (knex, Promise) {
 
 		.then(() => knex.schema.createTable('videos', (table) => {
 			table.increments();
-			table.integer('userid').references('users.id')
-			table.integer('gameid').references('games.id')
+			table.integer('created_by').references('users.id')
+			table.integer('game_id').references('games.id')
 			table.string('title', 100);
 			table.string('link', 255);
 			table.timestamps(false, true) // setting to Date-Time type value and using current time when saving
@@ -29,24 +29,24 @@ exports.up = function (knex, Promise) {
 
 		.then(() => knex.schema.createTable('streams', (table) => {
 			table.increments();
-			table.integer('userid').references('users.id');
-			table.integer('videoid').references('videos.id');
-			table.integer('gameid').references('games.id');
+			table.integer('created_by').references('users.id');
+			table.integer('video_id').references('videos.id');
+			table.integer('game_id').references('games.id');
 			table.string('title', 100);
 			table.timestamps(false, true);
 		}))
 
 		.then(() => knex.schema.createTable('followings', (table) => {
 			table.increments();
-			table.integer('userid').references('users.id'); //the person who is doing the following
-			table.integer('followingid').references('users.id'); //the user the person is following
+			table.integer('user').references('users.id'); //the person who is doing the following
+			table.integer('following').references('users.id'); //the user the person is following
 			table.timestamps(false, true);
 		}))
 
 		.then(() => knex.schema.createTable('comments', (table) => {
 			table.increments();
-			table.integer('userid').references('users.id');
-			table.integer('videoid').references('videos.id');
+			table.integer('created_by').references('users.id');
+			table.integer('video_id').references('videos.id');
 			table.string('comment', 255);
 			table.timestamps(false, true);
 		}))
