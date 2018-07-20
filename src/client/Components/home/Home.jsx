e8a08e6c8b2bcb5f9764c402feeca8461b924676
Unknown <cody.dillman13@gmail.com>
2018-07-20 16:00:58 -0600

    react-slick carousel / redux + redux-persist starterd

--- src/client/Components/home/Home.jsx
+++ src/client/Components/home/Home.jsx
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import Carousel from '../carousel/Carousel';
+
+const HomeOneDiv = styled.div`
+	background-color: #191B21;
+	width: 100%;
+	height: 100%;
+	> ::-webkit-scrollbar {
+		display: none;
+	}
+`;
+
+const HomeLiveContainer = styled.div`
+	width: 90%;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+`;
 
 export default class Home extends Component {
 	// constructor() {
@@ -14,9 +30,9 @@ export default class Home extends Component {
 
 	componentDidMount() {
 		// axios.get(`` /* all the carousel things */).then(res => {
-		// 	// this.setState({
-		// 	// 	carouselItems: res.data
-		// 	// })
+		// this.setState({
+		// 	carouselItems: res.data
+		// })
 		// });
 		// axios.get(`` /* all the live things */).then(res => {
 		// 	// this.setState({
@@ -24,17 +40,36 @@ export default class Home extends Component {
 		// 	// })
 		// });
 	}
-
+	
 	render() {
+		// const carouselItems = ['1','2','3','4']
+		const carouselItems2 = [{
+			picture: 'http://www.animalspot.net/wp-content/uploads/2012/05/Greater-White-Fronted-Goose-Images.jpg',
+			title: 'goose1'
+		},
+		{
+			picture: 'https://myronathon.files.wordpress.com/2012/04/canada-goose-solo-flying.png',
+			title: 'goose2'
+		},
+		{
+			picture : 'https://pmdvod.nationalgeographic.com/NG_Video_DEV/822/1022/goose_canada_480x360.jpg',
+			title: 'goose3'
+		},
+		{
+			picture : 'http://4.bp.blogspot.com/-SM7-1JmoFWM/UgFNona2Z4I/AAAAAAAAAYg/1MRseI-VEHM/s1600/goose.jpg',
+			title: 'goose4'
+		}]
 		return (
-			<Carousel title="carousel-item">
-				{/* this is where this.state.carouselItems will be rendered via a map */}
-
-				{/* this is where the cards by catagories will be rendered by a map */}
-			</Carousel>
+			<HomeOneDiv>
+				<Carousel carouselItems={carouselItems2}/>
+				<br/>
+				<br/>
+				<HomeLiveContainer>
+					{carouselItems2.map((goose) => (
+						<img style={{height: '10rem', width: '15rem', margin: '2rem'}} alt={goose.title} src={goose.picture}/>
+					)) }
+				</HomeLiveContainer>
+			</HomeOneDiv>
 		);
 	}
 }
-
-// //carousel build instructions that I followed came from here
-// //https://medium.com/@incubation.ff/build-your-own-css-carousel-in-react-part-one-86f71f6670ca
